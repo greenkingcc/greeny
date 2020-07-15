01c710340dd1c05033b9b868212cbfe322955d2c
greenking-lestrix <[gattinatorshop@gmail.com]>
2020-07-15 12:30:10 +0200

    Navbar added as well as basic css

---
+++ src/components/Globals/Navbar.js
@@ -0,0 +1,83 @@
+import React, { Component } from "react"
+import { Link } from "gatsby"
+import Logo from "../../images/logo2.png"
+import { IoIosMail } from "react-icons/io"
+
+export default class Navbar extends Component {
+  state = {
+    navbarOpen: false,
+    css: "collapse navbar-collapse",
+    links: [
+      {
+        id: 1,
+        path: "/",
+        text: "Über Uns",
+      },
+      {
+        id: 2,
+        path: "/objekte",
+        text: "Objekte",
+      },
+      {
+        id: 3,
+        path: "/archiv",
+        text: "archiv",
+      },
+      {
+        id: 4,
+        path: "/ankauf",
+        text: "Ankauf",
+      },
+      {
+        id: 5,
+        path: "/kontakt",
+        text: "Kontakt",
+      },
+      {
+        id: 6,
+        path: "/service",
+        text: "Service",
+      },
+    ],
+  }
+  navbarHandler = () => {
+    this.state.navbarOpen
+      ? this.setState({ navbarOpen: false, css: "collapse navbar-collapse" })
+      : this.setState({
+          navbarOpen: true,
+          css: "collapse navbar-collapse show",
+        })
+  }
+  render() {
+    return (
+      <nav className="navbar navbar-expand-sm bg-light navbar-light">
+        <Link to="/" className="navbar-brand">
+          <img src={Logo} alt="Logogattinator" />
+        </Link>
+        <button
+          className="navbar-toggler"
+          type="button"
+          onClick={this.bavbarHandler}
+        >
+          <span className="navbar-toggler-icon" />
+        </button>
+        <div className={this.state.css}>
+          <ul className="navbar-nav mx-auto">
+            {this.state.links.map(link => {
+              return (
+                <li key={link.id} className="nav-item">
+                  <Link to={link.path} className="nav-link text-capitalize">
+                    {link.text}
+                  </Link>
+                </li>
+              )
+            })}
+            <li className="nav-item ml-sm-5">
+              <IoIosMail className="mail-icon" />
+            </li>
+          </ul>
+        </div>
+      </nav>
+    )
+  }
+}
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,8 @@
 import React from "react"
 // import { Link } from "gatsby"
-
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 import { FaCouch } from "react-icons/fa"
-
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
