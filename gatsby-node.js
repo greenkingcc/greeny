279eede8f89d59297e89c631f57d6233e2ed4347
greenking-lestrix <[gattinatorshop@gmail.com]>
2020-07-17 06:13:18 +0200

    create pages programmatically

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,7 +1,42 @@
-/**
- * Implement Gatsby's Node APIs in this file.
- *
- * See: https://www.gatsbyjs.org/docs/node-apis/
- */
+// exports.createPages = async function({actions, graphql}) {
+//     const {data} = await graphql`
+//         query {
+//             contentfulProdukt {
+//                 slug
+//                 id
+//             }
+//         }
+//     `
 
-// You can delete this file if you're not using it
+// //create paginated pages for posts
+
+// const productsPerPage = 3
+
+// const numPages = Math.ceil(data.contentfulProdukt.edges.length/ productsPerPage)
+
+// Array.from({length: numPages}).forEach((_, i) => {
+//         actions.createPages({
+//             path: i==0 ? '/' : `/${i+1}`
+//             component: require.resolve("./src/templates/allProducts.js"),
+//             context: {
+//                 limit: productsPerPage,
+//                 skip: i * productsPerPage
+//                 numPages,
+//                 currentPage: i+i,
+//             }
+//         })
+//     })
+
+//     // Create single Product page
+
+//     data.contentfulProdukt.edges.forEach(edge => {
+//         const slug = edge.node.slug
+//         const id = edge.node.id
+//         actions.createPages({
+//             path: slug,
+//             component: require.resolve(`./src/templates/singleProduct.js`),
+//             context: {id}
+//         })
+//     })
+
+// }
