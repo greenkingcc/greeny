a3ddbb554edf488c629c62117e60519b8c728cee
greenking-lestrix <[gattinatorshop@gmail.com]>
2020-07-24 14:17:30 +0200

    showroom

--- src/components/Home/Products.js
+++ src/components/Home/Products.js
@@ -31,7 +31,7 @@ export default function Products() {
         return (
           <section className="py-5">
             <div className="container">
-              <Title title="Unsere Auswahl" />
+              <Title title="Unser Showroom" />
               <div className="row">
                 {data.products.edges.map(({ node: product }) => {
                   return <Product key={product.id} product={product} />;
